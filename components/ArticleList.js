308f4805ec37aa4625a693149ed6cdd6eb5445a1
codeForMobile <code.mobile@protonmail.com>
2022-02-05 16:02:36 +0200

    final commit

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -6,7 +6,7 @@ import ArticleItem from './ArticleItem';
 const ArticleList = ({articles}) => {
   return <div className={articleStyles.grid}>
       {articles.map(article => 
-        <ArticleItem article={article} />
+        <ArticleItem article={article} key={article.id} />
         )}
   </div>;
 };
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
-import Head from 'next/head'
 import ArticleList from '../components/ArticleList';
+import { server } from '../config/index'
 
 export default function Home({articles}) {
   console.log(articles);
   return (
     <div>
-      <Head>
-        <title>Webdev news</title>
-        <meta name='keywords' content='Web Development, programming'/>
-      </Head>
       <ArticleList articles={articles} />
     </div>
   )
 }
 
-export const getStaticProps = async () => {
+/* export const getStaticProps = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+  return {
+    props: {
+      articles
+    }
+  }
+} */
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
   return {
     props: {
